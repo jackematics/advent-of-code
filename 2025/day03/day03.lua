e7a928d514eea2b75f24e3b8adb75f4ffab36c04
Jackematics <jrimmer2996@gmail.com>
2025-12-03 17:31:22 +0000

    day 03 part 2 done

--- 2025/day03/day03.lua
+++ 2025/day03/day03.lua
@@ -17,8 +17,6 @@ for _, bank in ipairs(input) do
     end
   end
 
-  print('a', battery_a)
-
   local b_index = a_index + 1
   local battery_b = tonumber(string.sub(bank, b_index, b_index))
   for i = b_index, #bank do
@@ -29,14 +27,40 @@ for _, bank in ipairs(input) do
     end
   end
 
-  print('b', battery_b)
-
   local joltage = tonumber(tostring(battery_a) .. tostring(battery_b))
 
   output_joltage = output_joltage + joltage
-
-  print('joltage:', output_joltage)
-  print()
 end
 
 print('part 1:', output_joltage)
+
+-- PART 1
+
+local output_joltage_2 = 0
+
+for _, bank in ipairs(input) do
+  local batteries = ''
+
+  local best_i = 1
+
+  for limit = 11, 0, -1 do
+    local best_bat = tonumber(string.sub(bank, best_i, best_i))
+
+    for i = best_i, #bank - limit do
+      local battery = tonumber(string.sub(bank, i, i))
+
+      if battery > best_bat then
+        best_bat = battery
+        best_i = i
+      end
+    end
+
+    best_i = best_i + 1
+
+    batteries = batteries .. tostring(best_bat)
+  end
+
+  output_joltage_2 = output_joltage_2 + tonumber(batteries)
+end
+
+print('part 2:', output_joltage_2)
