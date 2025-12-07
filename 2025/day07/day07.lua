7347ce72435a3db7deb6d15385915d0e5fc2c4a6
Jackematics <jrimmer2996@gmail.com>
2025-12-07 17:14:00 +0000

    day 7 part 2 solution done but it's very slow, will try again with lua coroutines

--- 2025/day07/day07.lua
+++ 2025/day07/day07.lua
@@ -17,7 +17,7 @@ end
 -- PART 1
 
 local s_0 = { row = 1, col = string.find(input[1], 'S') }
-local split_count = 0
+local timeline_count = 0
 local covered = {}
 
 local function emit_beam(s_i)
@@ -26,7 +26,7 @@ local function emit_beam(s_i)
   end
 
   if string.sub(input[s_i.row], s_i.col, s_i.col) == '^' then
-    split_count = split_count + 1
+    timeline_count = timeline_count + 1
 
     local left = { row = s_i.row, col = s_i.col - 1 }
     local right = { row = s_i.row, col = s_i.col + 1 }
@@ -52,4 +52,31 @@ end
 
 emit_beam(s_0)
 
-print('part 1: ', split_count)
+print('part 1', timeline_count)
+
+-- PART 2
+
+s_0 = { row = 1, col = string.find(input[1], 'S') }
+timeline_count = 1
+
+local function emit_quantum_beam(s_i)
+  if string.sub(input[s_i.row], s_i.col, s_i.col) == '^' then
+    local left = { row = s_i.row, col = s_i.col - 1 }
+    local right = { row = s_i.row, col = s_i.col + 1 }
+
+    timeline_count = timeline_count + 1
+    emit_quantum_beam(left)
+    emit_quantum_beam(right)
+    return
+  end
+
+  if s_i.row == #input then
+    return
+  end
+
+  emit_quantum_beam { row = s_i.row + 1, col = s_i.col }
+end
+
+emit_quantum_beam(s_0)
+
+print('part 2', timeline_count)
